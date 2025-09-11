4b67fa62e5f5919586c42edd91e41e1c558555be
Rudra Verma <rudraverma781@gmail.com>
2025-09-11 12:30:27 +0530

    today task

---
+++ day-1/take_input.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+class take_input{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        System.out.println(s.nextInt());
+        System.out.println("Enter a word :");
+        System.out.println(s.next());
+        
+        
+    }
+}
