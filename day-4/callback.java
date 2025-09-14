0cda821113641db4cd6c959d0aad03d7a3d0b86b
Rudra Verma <rudraverma781@gmail.com>
2025-09-14 16:52:31 +0530

    today task

--- day-4/callback.java
+++ day-4/callback.java
@@ -14,4 +14,31 @@ class Customer implements  Member{
         System.out.println("Okay !!!!! I will come ~ "+ name);
     }
 }
+class Store{
+    Member m[] = new Member[100];
+    int c=0;
+    void register(Member m ){
+        this.m[c++]= m ;
+
+    }
+    void Invite(){
+        for(int i =0;i<c;i++){
+            m[i].callback();
+        }
+    }
+
+}
+class callback{
+    public static void main(String[] args) {
+        Customer c1 = new Customer("Rudra");
+        Customer c2 = new Customer("Alok");
+        Customer c3 = new Customer("Vinay");
+        Store s  = new Store();
+        s.register(c3);
+        s.register(c2);
+        s.register(c1);
+
+        s.Invite();
+    }
+}
 
