360e2bd4b6e2a4a9f6b5b7d923924f2917432771
Rudra Verma <rudraverma781@gmail.com>
2025-09-19 14:52:27 +0530

    today task

--- day-8/q10.java
+++ day-8/q10.java
@@ -1,1 +1,27 @@
 
+import java.util.Scanner;
+
+// reverse
+
+public class q10{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int arr[] = new int[5];
+        System.out.println("Enter 5 numbers");
+        for(int i=0;i<5;i++){
+            arr[i]=sc.nextInt();
+
+        }
+        for(int i=0;i<(arr.length/2);i++){
+            int t=arr[i];
+            arr[i]= arr[arr.length -i - 1];
+             arr[arr.length -i - 1] = t;
+
+        }
+
+        for(int x : arr){
+            System.out.print(" "+x);
+        }
+    }
+}
