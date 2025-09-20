72773d38adc7d2107b1e0eb13f168129393606d2
Rudra Verma <rudraverma781@gmail.com>
2025-09-20 13:33:43 +0530

    today task

---
+++ day-9/q9.java
@@ -0,0 +1,14 @@
+class q9{
+    public static void main(String[] args) {
+        int a=3,b=0;
+        try {
+            a=a/b;
+            System.out.println(" "+a);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+        finally{
+            System.out.println("Code Ended ");
+        }
+    }
+}
