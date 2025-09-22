39724b7cc40481490776a572f946e4e9db951705
Rudra Verma <rudraverma781@gmail.com>
2025-09-22 19:15:05 +0530

    today task

---
+++ day-11/q2.java
@@ -0,0 +1,46 @@
+
+// sync
+class mydata{
+    synchronized   void display(String s ){
+        for(char x : s.toCharArray()){
+            System.out.print(" "+x);
+        }
+    }
+}
+class Th1 extends Thread{
+    mydata d ;
+
+    public Th1(mydata d) {
+        this.d = d;
+    }
+    
+    public void run(){
+        d.display("ABCDEFGHIJKLSJSNDJDWHDKNCLMSIWNDKEMNOPQRSTUVWXYZ");
+    }
+
+}
+
+class Th2 extends Thread{
+    mydata d ;
+
+    public Th2(mydata d) {
+        this.d = d;
+    }
+    
+    public void run(){
+        d.display("01234567890275737377373828927383738373374744829282735848");
+    }
+    
+}
+
+
+class q2{
+    public static void main(String[] args) {
+        mydata d= new mydata();
+        Th1 t1 = new Th1(d);
+        Th2 t2= new Th2(d);
+          
+          t1.start();
+          t2.start();
+    }
+}
