35c79e73df2b488051e8bff31393ff41012abe2e
Rudra Verma <rudraverma781@gmail.com>
2025-09-22 19:21:22 +0530

    today task

---
+++ day-11/q4.java
@@ -0,0 +1,12 @@
+// object class 
+
+class q4{
+    public static void main(String[] args) {
+        
+        Object x  = new Object();
+        System.out.println("hash->"+x.hashCode());
+        System.out.println("tostring->"+x.toString());
+
+      System.out.println("Done ");
+    }
+}
