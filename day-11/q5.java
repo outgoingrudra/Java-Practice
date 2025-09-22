9687a9217af9d299091becd4ab0a1e23239d06f6
Rudra Verma <rudraverma781@gmail.com>
2025-09-22 19:24:18 +0530

    today task

---
+++ day-11/q5.java
@@ -0,0 +1,8 @@
+class q5{
+    public static void main(String[] args) {
+        Integer i = 100;
+        System.out.println("bytes->"+i.byteValue());
+        System.out.println("float->"+i.floatValue());
+        System.out.println("compare->"+i.compareTo(223));
+    }
+}
