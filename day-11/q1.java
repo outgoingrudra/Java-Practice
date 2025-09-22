c22bd655a49454a2ff4ea1101a9d86830a17f7cf
Rudra Verma <rudraverma781@gmail.com>
2025-09-22 19:13:27 +0530

    today task

--- day-11/q1.java
+++ day-11/q1.java
@@ -1,3 +1,5 @@
+
+// async
 class mydata{
     void display(String s ){
         for(char x : s.toCharArray()){
@@ -7,16 +9,26 @@ class mydata{
 }
 class Th1 extends Thread{
     mydata d ;
+
+    public Th1(mydata d) {
+        this.d = d;
+    }
+    
     public void run(){
-        d.display("Hello World !!!!!!!!!!!!");
+        d.display("ABCDEFGHIJKLSJSNDJDWHDKNCLMSIWNDKEMNOPQRSTUVWXYZ");
     }
 
 }
 
 class Th2 extends Thread{
     mydata d ;
+
+    public Th2(mydata d) {
+        this.d = d;
+    }
+    
     public void run(){
-        d.display("Welcome ");
+        d.display("01234567890275737377373828927383738373374744829282735848");
     }
     
 }
@@ -24,6 +36,11 @@ class Th2 extends Thread{
 
 class q1{
     public static void main(String[] args) {
-        
+        mydata d= new mydata();
+        Th1 t1 = new Th1(d);
+        Th2 t2= new Th2(d);
+          
+          t1.start();
+          t2.start();
     }
 }
