5ef06e17f7493fb26861ab4f9ec278e4752bdfae
Rudra Verma <rudraverma781@gmail.com>
2025-09-23 21:30:31 +0530

    today task

---
+++ day-12/q9.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class q9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+
+        double result = 0;
+        switch (op) {
+            case '+': result = a + b; break;
+            case '-': result = a - b; break;
+            case '*': result = a * b; break;
+            case '/': result = (b != 0) ? a / b : Double.NaN; break;
+            default: System.out.println("Invalid Operator!"); return;
+        }
+
+        System.out.println("Result = " + result);
+    }
+}
