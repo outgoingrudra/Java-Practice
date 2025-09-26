b9a80848a516dfdc36d43aa1a317ebbfd903bbd0
Rudra Verma <rudraverma781@gmail.com>
2025-09-26 11:09:18 +0530

    today task

---
+++ day-13/q1.java
@@ -0,0 +1,14 @@
+// enum
+enum dept {
+    CS , IT , CIVIL , ECE
+}
+
+public  class  q1{
+    public static void main(String[] args) {
+        dept d = dept.IT;
+        System.out.println("Ordinal -> "+d.ordinal());
+        System.out.println("ALL ENUM VALUES !!!!!!!!!");
+
+        
+    }
+}
