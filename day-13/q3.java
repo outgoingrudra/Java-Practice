5aa511c6358561cc42e669f3d4b9c86ee3d7a863
Rudra Verma <rudraverma781@gmail.com>
2025-09-26 11:15:27 +0530

    today task

---
+++ day-13/q3.java
@@ -0,0 +1,35 @@
+//switch case
+// enum
+enum dept {
+    CS , IT , CIVIL , ECE
+}
+
+public  class  q3{
+    public static void main(String[] args) {
+       
+      dept d= dept.CIVIL;
+
+      switch (d){
+          case CS:
+                System.out.println("Computer Science ");
+                break;
+         case IT:
+                System.out.println("Information Tch ");
+                break;
+         case CIVIL :
+            System.out.println("Civil ");
+            break;
+         case ECE:
+            System.out.println("ECE");
+            break;
+            
+              
+            
+          default:
+              throw new AssertionError();
+      }
+       
+
+        
+    }
+}
