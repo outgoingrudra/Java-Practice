1c32678b325ceb106d668f677e965796229c6ebb
Rudra Verma <rudraverma781@gmail.com>
2025-09-26 17:18:05 +0530

    today task

---
+++ day-13/q9.java
@@ -0,0 +1,13 @@
+@interface  abc{
+
+}
+
+@abc
+class q9{
+    @abc
+    int x = 5;
+    public static void main(String[] args) {
+        @abc int x=0;
+        System.out.println("My interface ");
+    }
+}
