c8641e0f9e625b8cb90511b9f9290c02a1f59382
Rudra Verma <rudraverma781@gmail.com>
2025-09-26 17:20:12 +0530

    today task

---
+++ day-13/q10.java
@@ -0,0 +1,15 @@
+@interface  abc{
+      String name() ;
+      String date() ;
+
+}
+
+@abc(name="Rudra",date="12/04/25")
+class q10{
+
+    int x = 5;
+    public static void main(String[] args) {
+         int x=0;
+        System.out.println("My interface ");
+    }
+}
