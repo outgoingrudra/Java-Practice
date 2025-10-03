64c4f4a596bd977e1aabd86877e9b492aeb167bd
Rudra Verma <rudraverma781@gmail.com>
2025-10-03 13:23:57 +0530

    today task

---
+++ day-17/q2.java
@@ -0,0 +1,22 @@
+
+class q2 {
+  static boolean isSorted(int arr[], int n) {
+
+    for(int i = 1; i < n ; i ++ ){
+        if(arr[i]<arr[i-1]){
+            return false ;
+        }
+    }
+        
+
+
+
+        return true;
+  }
+
+  public static void main(String args[]) {
+    int arr[] = {1, 2, 31, 4, 5}, n = 5;
+
+    System.out.println(isSorted(arr, n));
+  }
+}
